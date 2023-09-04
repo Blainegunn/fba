f060789beb8a5b1f3bd40024873c409828fd312c
Blaine Gunn <blainegunn@blue-book-air.lan>
2023-09-03 23:32:59 -0600

    ld

--- blocks/boot/boot.js
+++ blocks/boot/boot.js
@@ -1,15 +1,28 @@
 import { getLibs } from '../../scripts/utils.js';
+const { createTag } = await import(`${getLibs()}/utils/utils.js`);
+
 
 const labels = [
     'Brand', 'Model', 'Color', 'Style Code', 'Family', 'Year', 'Traction', 'MSRP', 'Credit'
 ]
 
+const  setTags =  async (ele, ts) => {
+  console.log((`setting tags for  ${ts}`));
+  ts.forEach((tag) => {
+
+    const tagLink = createTag('a', { href: `/${tag.trim()}`}, tag.trim());
+    console.log('tagLink');
+    console.log(ele.innerText);
+    ele.append(tagLink)
+    // ele.innerHTML = ele.innerHTML.replace(`${tag.trim()}`, tagLink)
+  });
+};
+
 // Add decorators for tags and brand names
 // NIKE tree of football
 
 export default async function init(el) {
-    const { createTag } = await import(`${getLibs()}/utils/utils.js`);
-    console.log(el);
+    // const { createTag } = await import(`${getLibs()}/utils/utils.js`);
     const details = createTag('div', { class: 'boot_details'});
     const children = Array.from(el.querySelectorAll(':scope > div'));
     // 1st, always img 
@@ -21,18 +34,22 @@ export default async function init(el) {
         lineItem.append(element);
         if (labels[idx] === 'Family') {
           const tags = element.childNodes[1].textContent.trim();
-          const metaItem = createTag('meta', { name: 'family', content: tags});
-	        document.body.append(metaItem);
           const tagsArr = tags.split(',');
-          console.log(tagsArr);
+          setTags(element, tagsArr);
 
-          tagsArr.forEach((tag) => {
-            console.log(tag.trim());
-            const tagLink = createTag('a', { href: `/${tag.trim()}`});
-            console.log('element');
-            console.log(element);
-            element = tagLink;
-          });
+
+
+          // const metaItem = createTag('meta', { name: 'family', content: tags});
+	        // document.body.append(metaItem);
+          // console.log(tagsArr);
+
+          // tagsArr.forEach((tag) => {
+
+          //   const tagLink = createTag('a', { href: `/${tag.trim()}`}, tag.trim());
+          //   console.log('tagLink');
+          //   console.log(element);
+          //   // element = tagLink;
+          // });
 
 
         }
